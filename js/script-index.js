bc8e503dd87320bf7926f60fbb3af870902ce32d
danamorah <damoraherrera@gmail.com>
2016-12-19 12:36:58 -0300

    add recipesArray iteration

--- js/script-index.js
+++ js/script-index.js
@@ -2,6 +2,12 @@ $(document).ready( function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
+    for (var i = 0 ; i < recipesArray.length ; i++){
+        // verificar highlited = true
+        if(recipesArray[i].highlighted === true ){
+            renderRecipe(this.recipesArray.highlighted);
+           }
+    }
 
 
 
